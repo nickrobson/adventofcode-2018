fc64e6b27262a85ba96029fdd474bd1ca7fd7021
Nick Robson <robson.nicholas1@gmail.com>
2018-12-02 23:52:23 +0100

    Simplify Day 01b a bit

--- day01/day01b.hs
+++ day01/day01b.hs
@@ -6,9 +6,8 @@ runLoop (x:xs) m i = do
   case M.lookup i m of
     -- this is 'Just 1' because by the time we get back here,
     -- we've seen i again (so it's actually the second time)
-    Just 1  -> i
-    Just n  -> runLoop xs (M.insert i (n + 1) m) (x + i)
-    Nothing -> runLoop xs (M.insert i 1       m) (x + i)
+    Just 1 -> i
+    _      -> runLoop xs (M.insertWith (+) i 1 m) (x + i)
 
 doRead :: String -> Int
 doRead ('+':s) = doRead s
